438e648976c3a56cb984b0ee6b776ba71befd839
Güney Ali Ünal <guneyunal@sabanciuniv.edu>
2021-11-05 19:50:53 +0000

    login.dart edited online with Bitbucket final version fixed navigation is complete for going back and forth

--- frontend/lib/routes/login.dart
+++ frontend/lib/routes/login.dart
@@ -15,9 +15,9 @@ class _LogInState extends State<LogIn> {
     final _key = GlobalKey<FormState>();
 
     // TODO: save user
-    void loginUser() {
+    void loginUser(){
       print("email: $email, password: $password");
-
+      Navigator.pushNamed(context, '/home');
       // Redirect to another page
     }
 
@@ -37,3 +37,83 @@ class _LogInState extends State<LogIn> {
                 Radius.circular(25),
               ),
             ),
+            child: Form(
+              key: _key,
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.all(40),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Login",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Container(
+                          width: 30,
+                          child: Divider(
+                            color: kPrimaryColor,
+                            thickness: 2,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'E-mail',
+                            labelText: 'E-mail',
+                            suffixIcon: Icon(
+                              Icons.mail_outline,
+                            ),
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Please don't leave e-mail address empty!";
+                            }
+                            if (!EmailValidator.validate(value)) {
+                              return "Please enter a valid e-mail address!";
+                            }
+
+                            return null;
+                          },
+                          onSaved: (value) {
+                            email = value;
+                          },
+                        ),
+                        SizedBox(
+                          height: 32,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Password',
+                            labelText: 'Password',
+                            suffixIcon: Icon(
+                              Icons.lock_outline,
+                            ),
+                          ),
+                          keyboardType: TextInputType.visiblePassword,
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Please don't leave password empty!";
+                            }
+                            if (value.length < 8) {
+                              return "Password must be at least 8 characters!";
+                            }
+                            return null;
+                          },
+                          onSaved: (value) {
+                            password = value;
+                          },
+                        ),
