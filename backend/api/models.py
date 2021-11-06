6a185dd94664c21b88ac823998f5bdee289a37b6
Enes Battal <52421169+m3n3s@users.noreply.github.com>
2021-11-06 18:43:14 +0300

    Created user model, serializer, and an endpoint for signing up. Added requirements.txt

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    userID = models.AutoField(primary_key=True, unique=True)
+    username = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    phoneNumber = models.CharField(max_length=20)
+    isAuthenticated = models.BooleanField(default=False)
+    isActive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import authentication, permissions, status
+from rest_framework.response import Response
+from .serializers import *
+import uuid
 
 # Create your views here.
+class SignUp(APIView):
+    permission_classes = [] # No permission needed
+    authentication_classes = [] # No authentication needed
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data, many=True)
+
+        if serializer.is_valid():
+            #TODO: Can hash the password and then save it
+
+            serializer.save()
+
+            return Response(
+                {
+                    "RequestId": str(uuid.uuid4()),
+                    "Message": "User created successfully",                    
+                    "User": serializer.data
+                }, 
+                status=status.HTTP_201_CREATED,
+            )
+
+        return Response(
+            {"Errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+    def get(self, request):
+        if User.objects.count != 0:
+            users = User.objects.all()
+            print("Userssssssss" , users)
+            serializer = UserSerializer(users, many=True)
+            return Response(serializer.data)
+
+        return Response(
+            {"Errors": "No user objects exist!"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
